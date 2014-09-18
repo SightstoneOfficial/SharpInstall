cbaf3aacdbffb46a5d4d1e6ae2aed98af71852fa
eddy5641 <eddydavinchi@gmail.com>
2014-09-18 14:56:27 -0700

    add missing files

---
+++ LCInstaller/Pages/CloseInstall.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LCInstaller.Pages
+{
+    /// <summary>
+    /// Interaction logic for CloseInstall.xaml
+    /// </summary>
+    public partial class CloseInstall : Page
+    {
+        public CloseInstall()
+        {
+            InitializeComponent();
+            Text.Text = "LegendaryClient is not finished installing" + Environment.NewLine +
+                "Are you sure you want to cancel the installation of LegendaryClient?";
+        }
+        private void Return(object sender, RoutedEventArgs e)
+        {
+            Logic.Logic.AbortQuit();
+        }
+        private void Quit(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ LCInstaller/TopBar.xaml.cs
@@ -0,0 +1,46 @@
+﻿using LCInstaller.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LCInstaller
+{
+    /// <summary>
+    /// Interaction logic for TopBar.xaml
+    /// </summary>
+    public partial class TopBar : UserControl
+    {
+        public TopBar()
+        {
+            InitializeComponent();
+            Label.Content = "LegendaryClient Installer";
+        }
+        private void DragGrid(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                Logic.Logic.Window.DragMove();
+        }
+        private void Minimize(object sender, RoutedEventArgs e)
+        {
+            Logic.Logic.Window.WindowState = WindowState.Minimized;
+        }
+        private void Close(object sender, RoutedEventArgs e)
+        {
+            if (!Logic.Logic.Installed)
+                Logic.Logic.SwichPage<CloseInstall>();
+            if (Logic.Logic.Installed)
+                Environment.Exit(0);
+        }
+    }
+}
